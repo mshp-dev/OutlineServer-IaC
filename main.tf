077a73ac58c0b49e8970a9bf069ea79f87628812
mshp-dev <m.shokri.software.engineer@gmail.com>
2024-12-27 18:22:27 +0330

    terraform apply and ansible playbook

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 }
 
 ## 1. custom VPC
-resource "aws_vpc" "arvan_sre_vpc" {
+resource "aws_vpc" "outline_vpc" {
   cidr_block         = "10.0.0.0/16"
   enable_dns_support = true
 
@@ -32,8 +32,8 @@ resource "aws_vpc" "arvan_sre_vpc" {
 }
 
 ## 2. subnet
-resource "aws_subnet" "arvan_sre_subnet" {
-  vpc_id                  = aws_vpc.arvan_sre_vpc.id
+resource "aws_subnet" "outline_subnet" {
+  vpc_id                  = aws_vpc.outline_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
@@ -43,8 +43,8 @@ resource "aws_subnet" "arvan_sre_subnet" {
 }
 
 ## 3. internet gateway
-resource "aws_internet_gateway" "arvan_sre_internet_gateway" {
-  vpc_id = aws_vpc.arvan_sre_vpc.id
+resource "aws_internet_gateway" "outline_internet_gateway" {
+  vpc_id = aws_vpc.outline_vpc.id
 
   tags = {
     Name = "igw_${var.name_postfix}"
@@ -52,12 +52,12 @@ resource "aws_internet_gateway" "arvan_sre_internet_gateway" {
 }
 
 ## 4. custom route table
-resource "aws_route_table" "arvan_sre_route_table" {
-  vpc_id = aws_vpc.arvan_sre_vpc.id
+resource "aws_route_table" "outline_route_table" {
+  vpc_id = aws_vpc.outline_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.arvan_sre_internet_gateway.id
+    gateway_id = aws_internet_gateway.outline_internet_gateway.id
   }
 
   tags = {
@@ -66,39 +66,22 @@ resource "aws_route_table" "arvan_sre_route_table" {
 }
 
 ## 5. associate the route table to the subnet
-resource "aws_route_table_association" "arvan_sre_rt_assc" {
-  subnet_id      = aws_subnet.arvan_sre_subnet.id
-  route_table_id = aws_route_table.arvan_sre_route_table.id
+resource "aws_route_table_association" "outline_rt_assc" {
+  subnet_id      = aws_subnet.outline_subnet.id
+  route_table_id = aws_route_table.outline_route_table.id
 }
 
 ## 6. create security groups
-### 1. common ports (ssh/http/https)
-resource "aws_security_group" "arvan_sre_security_group_common" {
+resource "aws_security_group" "outline_security_group_common" {
   name        = "sg_common_${var.name_postfix}"
-  description = "Allow SSH/HTTP/HTTPS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.arvan_sre_vpc.id
+  description = "Allow All inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.outline_vpc.id
 
   ingress {
-    description = "Allow SSH"
+    description = "Allow All"
     protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow HTTP"
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow HTTPS"
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
+    from_port   = 0
+    to_port     = 65535
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -113,223 +96,70 @@ resource "aws_security_group" "arvan_sre_security_group_common" {
     Name = "sg_common_${var.name_postfix}"
   }
 }
-### 2. k8s control-plane ports
-resource "aws_security_group" "arvan_sre_security_group_k8s_control_plane" {
-  name        = "sg_k8s_cp_${var.name_postfix}"
-  description = "K8S Control Plane Security Group"
-  vpc_id      = aws_vpc.arvan_sre_vpc.id
-
-  ingress {
-    description = "Kubernetes API Server"
-    protocol    = "tcp"
-    from_port   = 6443
-    to_port     = 6443
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Etcd server client API"
-    protocol    = "tcp"
-    from_port   = 2379
-    to_port     = 2380
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Kubelet API"
-    protocol    = "tcp"
-    from_port   = 10250
-    to_port     = 10250
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Kube Scheduler"
-    protocol    = "tcp"
-    from_port   = 10259
-    to_port     = 10259
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Kube Contoller Manager"
-    protocol    = "tcp"
-    from_port   = 10257
-    to_port     = 10257
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "sg_k8s_cp_${var.name_postfix}"
-  }
-}
-### 3. k8s worker-node ports
-resource "aws_security_group" "arvan_sre_security_group_k8s_worker_nodes" {
-  name        = "sg_k8s_wn_${var.name_postfix}"
-  description = "K8S Worker Nodes Security Group"
-  vpc_id      = aws_vpc.arvan_sre_vpc.id
-
-  ingress {
-    description = "Kubelet API"
-    protocol    = "tcp"
-    from_port   = 10250
-    to_port     = 10250
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Kube Proxy"
-    protocol    = "tcp"
-    from_port   = 10256
-    to_port     = 10256
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "NodePort Servicest"
-    protocol    = "tcp"
-    from_port   = 30000
-    to_port     = 32767
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "sg_k8s_wn_${var.name_postfix}"
-  }
-}
-### 4. k8s flannel UDP backend ports
-resource "aws_security_group" "arvan_sre_security_group_k8s_flannel" {
-  name        = "sg_k8s_flnl_${var.name_postfix}"
-  description = "K8S Flannel Overlay backend"
-  vpc_id      = aws_vpc.arvan_sre_vpc.id
-
-  ingress {
-    description = "flannel overlay backend"
-    protocol    = "udp"
-    from_port   = 8285
-    to_port     = 8285
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "flannel vxlan backend"
-    protocol    = "udp"
-    from_port   = 8472
-    to_port     = 8472
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "sg_k8s_flnl_${var.name_postfix}"
-  }
-}
 
 ## 7. create instances for k8s cluster
 ### 1. create key-pair for password-less ssh
-resource "tls_private_key" "arvan_sre_ssh_rsa_private_key" {
+resource "tls_private_key" "outline_ssh_rsa_private_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 
   provisioner "local-exec" {
-    command = "echo '${self.public_key_pem}' > ./files/arvan_sre_rsa.pub"
+    command = "echo '${self.public_key_pem}' > ./files/outline_rsa.pub"
   }
 }
-resource "aws_key_pair" "arvan_sre_ssh_keypair" {
+resource "aws_key_pair" "outline_ssh_keypair" {
   key_name   = var.ssh_key_pair_name
-  public_key = tls_private_key.arvan_sre_ssh_rsa_private_key.public_key_openssh
+  public_key = tls_private_key.outline_ssh_rsa_private_key.public_key_openssh
 
   provisioner "local-exec" {
-    command = "echo '${tls_private_key.arvan_sre_ssh_rsa_private_key.private_key_pem}' > ./files/arvan_sre_rsa"
+    command = "echo '${tls_private_key.outline_ssh_rsa_private_key.private_key_pem}' > ./files/outline_rsa"
+  }
+
+  provisioner "local-exec" {
+    command = "chmod 400 ./files/outline_rsa"
   }
 }
 ### 2. create 1/one ec2 instance to play as k8s control plane
-resource "aws_instance" "arvan_sre_ec2_k8s_master" {
-  ami                         = var.ubuntu_ami
-  instance_type               = "t2.medium"
-  key_name                    = aws_key_pair.arvan_sre_ssh_keypair.key_name
-  subnet_id                   = aws_subnet.arvan_sre_subnet.id
-  associate_public_ip_address = true
-
-  vpc_security_group_ids = [
-    aws_security_group.arvan_sre_security_group_common.id,
-    aws_security_group.arvan_sre_security_group_k8s_control_plane.id,
-    aws_security_group.arvan_sre_security_group_k8s_flannel.id,
-  ]
-
-  root_block_device {
-    volume_type = "gp3"
-    volume_size = 14
-  }
-
-  tags = {
-    Name = "ec2_k8s_master_${var.name_postfix}"
-    Role = "control_plane, master"
-  }
-
-  provisioner "local-exec" {
-    command = "echo 'master ${self.public_ip}' >> ./files/hosts"
-  }
-}
-### 3. create 2/two ec2 instances to play as k8s worker nodes
-resource "aws_instance" "arvan_sre_ec2_k8s_workers" {
-  count                       = var.worker_counts
+resource "aws_instance" "outline_server_ec2" {
   ami                         = var.ubuntu_ami
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.arvan_sre_ssh_keypair.key_name
-  subnet_id                   = aws_subnet.arvan_sre_subnet.id
+  key_name                    = aws_key_pair.outline_ssh_keypair.key_name
+  subnet_id                   = aws_subnet.outline_subnet.id
   associate_public_ip_address = true
 
   vpc_security_group_ids = [
-    aws_security_group.arvan_sre_security_group_common.id,
-    aws_security_group.arvan_sre_security_group_k8s_worker_nodes.id,
-    aws_security_group.arvan_sre_security_group_k8s_flannel.id,
+    aws_security_group.outline_security_group_common.id,
   ]
 
   root_block_device {
     volume_type = "gp3"
-    volume_size = 8
+    volume_size = 10
   }
 
   tags = {
-    Name = "ec2_k8s_worker_${count.index}_${var.name_postfix}"
-    Role = "worker"
+    Name = "ec2_${var.name_postfix}"
+    Role = "server"
   }
 
   provisioner "local-exec" {
-    command = "echo 'worker-${count.index} ${self.public_ip}' >> ./files/hosts"
+    command = "echo 'outline-server ${self.public_ip}' >> ./files/hosts"
   }
 }
 
 ## 8. create host inventory for k8s control plane and worker nodes
-resource "ansible_host" "arvan_sre_control_plane_host" {
+resource "ansible_host" "outline_server_host" {
   depends_on = [
-    aws_instance.arvan_sre_ec2_k8s_master
+    aws_instance.outline_server_ec2
   ]
 
-  name   = "control_plane"
-  groups = ["master"]
+  name   = "outline_server"
+  groups = ["server"]
 
   variables = {
     ansible_user                 = "ubuntu"
-    ansible_host                 = aws_instance.arvan_sre_ec2_k8s_master.public_ip
-    ansible_ssh_private_key_file = "./files/arvan_sre_rsa"
-    node_hostname                = "master"
-  }
-}
-resource "ansible_host" "arvan_sre_worker_nodes_host" {
-  depends_on = [
-    aws_instance.arvan_sre_ec2_k8s_workers
-  ]
-
-  count  = var.worker_counts
-  name   = "worker-${count.index}"
-  groups = ["workers"]
-
-  variables = {
-    node_hostname                = "worker-${count.index}"
-    ansible_user                 = "ubuntu"
-    ansible_host                 = aws_instance.arvan_sre_ec2_k8s_workers[count.index].public_ip
-    ansible_ssh_private_key_file = "./files/arvan_sre_rsa"
+    ansible_host                 = aws_instance.outline_server_ec2.public_ip
+    ansible_ssh_private_key_file = "./files/outline_rsa"
+    node_hostname                = "server"
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,29 +1,23 @@
 variable "name_postfix" {
   type        = string
   description = "The postfix part of all objects name"
-  default     = "arvan_sre"
+  default     = "outline_server"
 }
 
 variable "ssh_key_pair_name" {
   type        = string
   description = "The name of SSH key-pair"
-  default     = "arvan_sre_ssh_key"
+  default     = "outline_ssh_key"
 }
 
 variable "aws_region" {
   type        = string
   description = "The AWS region"
-  default     = "us-east-1"
+  default     = "ca-central-1"
 }
 
 variable "ubuntu_ami" {
   type        = string
   description = "The Ubuntu 24.04 ami"
-  default     = "ami-0e2c8caa4b6378d8c"
-}
-
-variable "worker_counts" {
-  type        = number
-  description = "Count of k8s cluster worker nodes"
-  default     = 2
+  default     = "ami-0bee12a638c7a8942"
 }
